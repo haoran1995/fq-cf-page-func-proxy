643a659d8cedda6fcdd7ffc2bafc6a1d064b7b76
haoran1995 <80933126+haoran1995@users.noreply.github.com>
2022-11-03 17:03:34 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -2,7 +2,7 @@ export default {
     async fetch(request, env) {
       let url = new URL(request.url);
       if (url.pathname.startsWith('/')) {
-        url.hostname="v-220104.herokuapp.com";
+        url.hostname="1103.200617.xyz";
         let new_request=new Request(url,request);
         return fetch(new_request);
       }
