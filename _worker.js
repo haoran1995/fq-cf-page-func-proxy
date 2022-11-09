e8d4b1c32868e270790ce98b5b4ce1f6c0a9e009
haoran1995 <80933126+haoran1995@users.noreply.github.com>
2022-11-09 20:28:26 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -2,7 +2,7 @@ export default {
     async fetch(request, env) {
       let url = new URL(request.url);
       if (url.pathname.startsWith('/')) {
-        url.hostname="fq.221104.tk:2053";
+        url.hostname="cf.2238326315.workers.dev";
         let new_request=new Request(url,request);
         return fetch(new_request);
       }
